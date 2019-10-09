29b44b8a0b77fade92eb4efd067fb5abeac32f0c
Donny Xia <donny.xia@marklogic.com>
2019-10-09 10:25:09 -0700

    Register slave node

--- master/master.py
+++ master/master.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+import logging
 
 app = Flask(__name__)
 
+slaves = []
+
 @app.route("/")
 def hello():
-    return "Master"
+    return str(slaves)
+
+@app.route("/register-slave" ,methods=["GET"])
+def register_slave():
+    host = request.args.get("host", "")
+    port = request.args.get("port", "")
+    logging.info("Register {}:{}".format(host, port))
+    global slaves
+    if (host, port) not in slaves:
+        slaves.append((host, port))
+    return "ok"
--- slave/slave.py
+++ slave/slave.py
@@ -1,7 +1,30 @@
 from flask import Flask
+import requests
+import socket
+import logging
+logging.getLogger().setLevel(logging.INFO)
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Slave"
+
+def register_to_master():
+    # Register to master
+    master_host = "cnt_master"
+    master_port = 5999
+    url = "http://{}:{}/register-slave".format(master_host, master_port)
+    self_hostname = socket.getfqdn()
+    app.logger.info("Register self: {}:{} to {}".format(self_hostname, 6000, url))
+    r = requests.get(
+        url=url,
+        params={
+            "host": self_hostname,
+            "port": 6000
+        }
+    )
+    if not r.ok:
+        app.logger.error("Failed to register. Return {}. Body:\n{}".format(r.status_code, r.text))
+
+register_to_master()
